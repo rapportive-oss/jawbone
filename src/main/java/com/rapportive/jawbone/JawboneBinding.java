6c38a535146277c8f6bc5565c9ad3445c884b3fa
Sam Stokes <me@samstokes.co.uk>
2012-05-23 14:39:00 -0700

    Rudimentary binding for wbxml_conv_wbxml2xml_run

---
+++ src/main/java/com/rapportive/jawbone/JawboneBinding.java
@@ -0,0 +1,22 @@
+package com.rapportive.jawbone;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+import com.sun.jna.ptr.LongByReference;
+import com.sun.jna.ptr.PointerByReference;
+
+public interface JawboneBinding extends Library {
+    JawboneBinding INSTANCE = (JawboneBinding) Native.loadLibrary("wbxml2", JawboneBinding.class);
+
+    int wbxml_conv_wbxml2xml_create(PointerByReference jawbonePtr);
+
+    void wbxml_conv_wbxml2xml_set_gen_type(Pointer jawbone, int genType);
+    void wbxml_conv_wbxml2xml_set_language(Pointer jawbone, int language);
+    void wbxml_conv_wbxml2xml_set_charset(Pointer jawbone, int charset);
+    void wbxml_conv_wbxml2xml_set_indent(Pointer jawbone, int indent);
+    void wbxml_conv_wbxml2xml_enable_preserve_whitespaces(Pointer jawbone);
+
+    int wbxml_conv_wbxml2xml_run(Pointer jawbone, byte[] wbxml, long wbxmlLength, PointerByReference xmlPtr, LongByReference xmlLength);
+}
